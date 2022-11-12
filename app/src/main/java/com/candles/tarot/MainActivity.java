e9a15a8efa060f74012b94e29df7a496edc90baa
TranHoang635 <lykhoa2112@gmail.com>
2022-11-13 02:59:15 +0700

    hoangvinh 13/11

--- app/src/main/java/com/candles/tarot/MainActivity.java
+++ app/src/main/java/com/candles/tarot/MainActivity.java
@@ -7,18 +7,15 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 public class MainActivity extends AppCompatActivity {
 
-
+//    TextView txtHienthithongdiep;
     Button btn_join, btn_exit;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addControls();
-
         addEvent();
     }
 
@@ -30,20 +27,41 @@ public class MainActivity extends AppCompatActivity {
                 System.exit(1);
             }
         });
+
         //-------------button join-------------//
         btn_join.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, MainActivity2.class);
                 startActivity(intent);
-                Toast.makeText(MainActivity.this, "Đã vào tarot ! hãy thử rút một lá bài.", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "Tarot run!!! Hãy thử rút một lá bài.", Toast.LENGTH_LONG).show();
             }
         });
+
+//        btn_rut.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity2.this,MainActivity3.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        btn_chon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity2.this,MainActivityCard.class);
+//                startActivity(intent);
+//            }
+//        });
     }
 
     private void addControls() {
         btn_join = findViewById(R.id.btn_join);
         btn_exit = findViewById(R.id.btn_exit);
+//        btn_rut = findViewById(R.id.btnRut);
+//        btn_chon = findViewById(R.id.btn_Chon);
+//        txtHienthithongdiep = findViewById(R.id.txtHienthithongdiep);
     }
 }
+
 
